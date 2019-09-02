fe01a032e2d4782ab23e52927159d6e87e8935c5
Kishor Naik <kishor.naik011.net@gmail.com>
2019-09-02 16:40:02 +0530

    Code Commit

---
+++ Sol_Html_Action_Url_Action/Sol_Html_Action_Url_Action/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sol_Html_Action_Url_Action.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
